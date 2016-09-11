e4cb540dc10716b91a2beb4d53663d516156472a
Simon Rüegg <simon@rueggs.ch>
2016-09-11 16:38:02 -0400

    basic string processing

--- meter.c
+++ meter.c
@@ -1,25 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
+#include <stdbool.h>
+
+#include "meter.h"
 
 
+bool _verbose = false;
 
 int main(int argc, char *argv[]) {
 
   uint32_t window_size = 32;
   char *input_file = NULL;
-  int c;
 
   if(argc <= 1){
-    printf("Usage: meter [-w WINDOW_SIZE] INPUT_FILE\n");
+    printf("Usage: meter [-w WINDOW_SIZE] [-v] INPUT_FILE\n");
     return -1;
   }
   opterr = 0;
+  int c;
   while ((c = getopt (argc, argv, "w:")) != -1){
     switch (c) {
+      case 'v':
+        _verbose = true;
+      break;
       case 'w':
         if(sscanf(optarg, "%d", &window_size) <= 0){
           fprintf(stderr, "window_size must be a positive number.\n");
@@ -48,12 +54,63 @@ int main(int argc, char *argv[]) {
     return -1;
   }
 
+  return process_file(input_file, window_size);
+}
 
+int process_file(char *input_file, uint32_t window_size){
+  
   FILE *fp;
+  char *file_content;
+  long length;
 
-  printf("input file: %s, window size: %i\n", input_file, window_size);
-
+  if(_verbose){
+    printf("Open file: %s\n", input_file);
+  }
   fp = fopen(input_file, "r");
+  if(fp){
+    fseek (fp, 0, SEEK_END);
+    length = ftell (fp);
+    fseek (fp, 0, SEEK_SET);
+    file_content = malloc (length);
+    if (file_content)
+    {
+      fread (file_content, 1, length, fp);
+    }
+    fclose(fp);
+  }else{
+    fprintf(stderr, "Could not open file '%s\'\n", input_file);
+    return -2;
+  }
 
-  fclose(fp);
+  if(file_content == NULL){
+    fprintf(stderr, "No data read from file '%s'\n", input_file);
+    return -2;
+  }
+
+  remove_nonalpha(file_content);
+
+  for(int i = 0; file_content[i]; i++){
+    file_content[i] = tolower(file_content[i]);
+  }
+  
+  printf("%s", file_content);
+
+  return 0;
+}
+
+
+void remove_nonalpha(char *str)
+{
+    unsigned long i = 0;
+    unsigned long j = 0;
+    char c;
+
+    while ((c = str[i++]) != '\0')
+    {
+        if (isalpha(c) || c == ' ')
+        {
+            str[j++] = c;
+        }
+    }
+    str[j] = '\0';
 }
---
+++ meter.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H_   /* Include guard */
+#define MAIN_H_
+
+int process_file(char *, uint32_t);
+void remove_nonalpha(char *);
+
+#endif // MAIN_H_

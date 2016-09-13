f101c42929b91e93e1f89f5e8afa262be462fb56
Simon Rüegg <simon@rueggs.ch>
2016-09-13 15:38:38 -0400

    line by line processing

--- meter.c
+++ meter.c
@@ -25,24 +25,24 @@ int main(int argc, char *argv[]) {
   while ((c = getopt (argc, argv, "w:v")) != -1){
     switch (c) {
       case 'v':
-        verbose = true;
+      verbose = true;
       break;
       case 'w':
-        if(sscanf(optarg, "%d", &window_size) <= 0){
-          fprintf(stderr, "window_size must be a positive number.\n");
-          return -1;
-        }
-        break;
+      if(sscanf(optarg, "%d", &window_size) <= 0){
+        fprintf(stderr, "window_size must be a positive number.\n");
+        return -1;
+      }
+      break;
       case '?':
-        if (optopt == 'w')
-          fprintf (stderr, "Option -%c requires an argument.\n", optopt);
-        else if (isprint (optopt))
-          fprintf (stderr, "Unknown option `-%c'.\n", optopt);
-        else
-          fprintf (stderr,
-           "Unknown option character `\\x%x'.\n",
-           optopt);
-        return 1;
+      if (optopt == 'w')
+        fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+      else if (isprint (optopt))
+        fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+      else
+        fprintf (stderr,
+         "Unknown option character `\\x%x'.\n",
+         optopt);
+      return 1;
       default:
       abort ();
     }
@@ -71,62 +71,54 @@ int main(int argc, char *argv[]) {
  */
 
 int process_file(char *input_file, uint32_t window_size){
-  
+
   FILE *fp;
-  char *file_content;
-  long length;
+  uint32_t total_word_count = 0;
 
   if(verbose){
     printf("Open file: %s.\n", input_file);
   }
   fp = fopen(input_file, "r");
   if(fp){
-    fseek(fp, 0, SEEK_END);
-    length = ftell(fp);
-    fseek(fp, 0, SEEK_SET);
-    file_content = malloc (length);
-    if (file_content)
-    {
-      fread(file_content, 1, length, fp);
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    
+
+    /* read file line by line */
+    while ((read = getline(&line, &len, fp)) != -1) {
+      if(line == NULL){
+        fprintf(stderr, "No data read from file '%s'.\n", input_file);
+        return -2;
+      }
+
+      /* remove non-alphabetic characters from text */
+      remove_nonalpha(line);
+      /* convert all text to lowercase */
+      for(int i = 0; line[i]; i++){
+        line[i] = tolower(line[i]);
+      }
+      
+      char *word = strtok(line," ");
+      /* process line word by word */
+      while(word != NULL){
+        increase_word_count(&word_counters_local, word);
+        increase_word_count(&word_counters_total, word);
+        total_word_count++;
+        if(total_word_count % window_size == 0){
+          /* calculate local complexity for current window */
+          double local_complexity = (double)get_word_count(&word_counters_local) / window_size;
+          printf("Window %i: local complexity: %f\n", total_word_count/window_size, local_complexity);
+          clear_word_counts(&word_counters_local);
+        }
+        word = strtok(NULL, " ");
+      }
     }
+    free(line);
     fclose(fp);
   }else{
     fprintf(stderr, "Could not open file '%s\'.\n", input_file);
     return -2;
-  }
-
-  if(file_content == NULL){
-    fprintf(stderr, "No data read from file '%s'.\n", input_file);
-    return -2;
-  }
-
-  /* remove non-alphabetic characters from text */
-  remove_nonalpha(file_content);
-
-  /* convert all text to lowercase */
-  for(int i = 0; file_content[i]; i++){
-    file_content[i] = tolower(file_content[i]);
-  }
-  if(verbose){
-    printf("Cleaned file content.\n");
-  }
-
-  /* process text word by word */
-
-  uint32_t total_word_count = 0;
-  char *word = strtok(file_content," ");
-   
-  while(word != NULL){
-    increase_word_count(&word_counters_local, word);
-    increase_word_count(&word_counters_total, word);
-    total_word_count++;
-    if(total_word_count % window_size == 0){
-      /* calculate local complexity for current window */
-      double local_complexity = (double)get_word_count(&word_counters_local) / window_size;
-      printf("Window %i: local complexity: %f\n", total_word_count/window_size, local_complexity);
-      clear_word_counts(&word_counters_local);
-    }
-    word = strtok(NULL, " ");
   }
 
   uint32_t distinct_word_count = get_word_count(&word_counters_total);
@@ -135,7 +127,7 @@ int process_file(char *input_file, uint32_t window_size){
 
   printf("Distinct words: %i, total words: %i, total complexity: %f\n", distinct_word_count, total_word_count, total_complexity);
 
-  return 0;
+  return EXIT_SUCCESS;
 }
 
 
